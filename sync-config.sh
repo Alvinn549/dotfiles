95555e390d3fa56c683ae106725b1ccaf56721de
Alvin Nuha M <alvinn549@gmail.com>
2024-12-24 09:44:29 +0700

    change lock time, add blueman startup and floating point

--- sync-config.sh
+++ sync-config.sh
@@ -3,6 +3,7 @@
 # Paths
 CONFIG_DIR="$HOME/.config"
 REPO_DIR="$HOME/Projects/dotfiles/.config"
+ROOT_REPO="$HOME/Projects/dotfiles"
 
 # Directories to sync
 DIRECTORIES=("sway" "waybar" "alacritty" "neofetch" "qt5ct" "scripts" "swaync" "wlogout" "wofi")
@@ -22,25 +23,48 @@ sync_directory() {
   echo "$1 synced successfully."
 }
 
+sync_file() {
+  local source_file="$1"
+  local target_file="$2"
+
+  echo "Syncing $source_file to $target_file..."
+
+  # Ensure the target directory exists
+  mkdir -p "$(dirname "$target_file")"
+
+  # Sync the file
+  rsync -av "$source_file" "$target_file"
+
+  echo "$(basename "$source_file") synced successfully."
+}
+
 echo "Starting config sync..."
 for dir in "${DIRECTORIES[@]}"; do
   sync_directory "$dir"
 done
 
-# Paths
-SOURCE_FILE="$HOME/.config/Code/User/settings.json"
-TARGET_FILE="$HOME/Projects/dotfiles/.config/Code/User/settings.json"
-
-# Ensure the target directory exists
-TARGET_DIR=$(dirname "$TARGET_FILE")
-mkdir -p "$TARGET_DIR"
-
-echo "Syncing $SOURCE_FILE to $TARGET_FILE..."
-
-# Sync the file
-rsync -av "$SOURCE_FILE" "$TARGET_FILE"
-
-# Sync ~/.zshrc to Projects/dotfiles/.zshrc
-cp ~/.zshrc /home/alvin/Projects/dotfiles/.zshrc
+# Additional files to sync
+sync_file "$HOME/.config/Code/User/settings.json" "$HOME/Projects/dotfiles/.config/Code/User/settings.json"
+sync_file "$HOME/.zshrc" "$ROOT_REPO/.zshrc"
 
 echo "Config sync completed."
+
+# Check for changes in the Git repository
+cd "$ROOT_REPO" || { echo "Failed to change directory to $ROOT_REPO"; exit 1; }
+
+if git diff-index --quiet HEAD --; then
+  echo "No changes detected. Exiting."
+else
+  echo "Changes detected. Preparing to commit."
+  git status
+
+  # Prompt user for commit message
+  read -rp "Enter your commit message: " COMMIT_MSG
+
+  # Stage, commit, and push changes
+  git add .
+  git commit -m "$COMMIT_MSG"
+  git push origin main
+
+  echo "Changes pushed successfully."
+fi

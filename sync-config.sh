bb62082de4a3212f98b6b20618c55979ab695575
Alvin Nuha M <alvinn549@gmail.com>
2024-12-24 09:47:46 +0700

    add script to sync wallpapers

--- sync-config.sh
+++ sync-config.sh
@@ -4,13 +4,15 @@
 CONFIG_DIR="$HOME/.config"
 REPO_DIR="$HOME/Projects/dotfiles/.config"
 ROOT_REPO="$HOME/Projects/dotfiles"
+WALLPAPER_SOURCE_DIR="$HOME/Pictures/Wallpapers"
+WALLPAPER_TARGET_DIR="$ROOT_REPO/Wallpapers"
 
-# Directories to sync
+# Config directories to sync
 DIRECTORIES=("sway" "waybar" "alacritty" "neofetch" "qt5ct" "scripts" "swaync" "wlogout" "wofi")
 
 sync_directory() {
-  local source_dir="$CONFIG_DIR/$1"
-  local target_dir="$REPO_DIR/$1"
+  local source_dir="$1"
+  local target_dir="$2"
 
   echo "Syncing $source_dir to $target_dir..."
 
@@ -20,7 +22,7 @@ sync_directory() {
   # Sync files (delete removed files, copy new and modified ones)
   rsync -av --delete "$source_dir/" "$target_dir/"
 
-  echo "$1 synced successfully."
+  echo "$(basename "$source_dir") synced successfully."
 }
 
 sync_file() {
@@ -39,15 +41,20 @@ sync_file() {
 }
 
 echo "Starting config sync..."
+
 for dir in "${DIRECTORIES[@]}"; do
-  sync_directory "$dir"
+  sync_directory "$CONFIG_DIR/$dir" "$REPO_DIR/$dir"
 done
 
-# Additional files to sync
-sync_file "$HOME/.config/Code/User/settings.json" "$HOME/Projects/dotfiles/.config/Code/User/settings.json"
+# Sync additional files
+sync_file "$HOME/.config/Code/User/settings.json" "$ROOT_REPO/.config/Code/User/settings.json"
 sync_file "$HOME/.zshrc" "$ROOT_REPO/.zshrc"
 
-echo "Config sync completed."
+# Sync Wallpapers
+echo "Syncing Wallpapers..."
+sync_directory "$WALLPAPER_SOURCE_DIR" "$WALLPAPER_TARGET_DIR"
+
+echo "Config and Wallpaper sync completed."
 
 # Check for changes in the Git repository
 cd "$ROOT_REPO" || { echo "Failed to change directory to $ROOT_REPO"; exit 1; }
